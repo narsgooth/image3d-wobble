ca95950f16d614eed16505828787f23df765006c
narsgooth <l4me.duck@gmail.com>
2025-06-28 07:03:51 -0500

    scale factor + box

--- js/keyboard.js
+++ js/keyboard.js
@@ -9,4 +9,24 @@ document.addEventListener('keydown', function(event) {
 	else if (event.key === 'r' || event.key === 'R') {
         randomize();
     }
+	else if (event.key === 'w' || event.key === 'W') {
+        scaleW += .1;
+		document.getElementById('scaleW').value = scaleW;
+    }
+	else if (event.key === 's' || event.key === 'S') {
+        scaleW -= .1;
+		document.getElementById('scaleW').value = scaleW;
+    }
+	else if (event.key === 'd' || event.key === 'D') {
+        frequencyW += 1;
+		document.getElementById('frequencyW').value = frequencyW;
+    }
+	else if (event.key === 'a' || event.key === 'A') {
+        frequencyW -= 1;
+		document.getElementById('frequencyW').value = frequencyW;
+    }
+	else if (event.key === 'b' || event.key === 'B') {
+        boxVisible =!boxVisible;
+		document.getElementById('boxVisible').checked = boxVisible;
+    }
   });
